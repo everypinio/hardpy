775960880f7f8b9a98bd19ac560f2286b54cf261
i.alexandrov <i.alexandrov@thirdpin.io>
2024-03-18 10:44:28 +0300

    [tests] Add default case and module tests

--- tests/test_mark.py
+++ tests/test_mark.py
@@ -13,6 +13,13 @@ mark_test_header = """
         pytestmark = pytest.mark.module_name(module_name)
         """
 
+test_default_header = """
+        import pytest
+
+        from hardpy import get_current_report
+        from hardpy.pytest_hardpy.utils import NodeInfo
+        """
+
 
 def test_module_name(hardpy_init, pytester: Pytester):
     pytester.makepyfile(
@@ -40,6 +47,36 @@ def test_case_name(hardpy_init, pytester: Pytester):
             report = get_current_report()
             name = report.modules[node.module_id].cases[node.case_id].name
             assert case_name == name
+    """
+    )
+    result = pytester.runpytest(hardpy_dbh(), hardpy_connect_db())
+    result.assert_outcomes(passed=1)
+
+
+def test_default_case_name(hardpy_init, pytester: Pytester):
+    pytester.makepyfile(
+        f"""
+        {test_default_header}
+        def test_check_default_case_name(request):
+            node = NodeInfo(request.node)
+            report = get_current_report()
+            name = report.modules[node.module_id].cases[node.case_id].name
+            assert name == "test_check_default_case_name"
+    """
+    )
+    result = pytester.runpytest(hardpy_dbh(), hardpy_connect_db())
+    result.assert_outcomes(passed=1)
+
+
+def test_default_module_name(hardpy_init, pytester: Pytester):
+    pytester.makepyfile(
+        f"""
+        {test_default_header}
+        def test_check_default_module_name(request):
+            node = NodeInfo(request.node)
+            report = get_current_report()
+            read_module_name = report.modules[node.module_id].name
+            assert read_module_name == "test_default_module_name"
     """
     )
     result = pytester.runpytest(hardpy_dbh(), hardpy_connect_db())
