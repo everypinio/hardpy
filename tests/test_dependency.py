680c5a9f3161737893e25422e507b268d78350b3
m.antonovich <m.antonovich@thirdpin.io>
2024-05-28 15:39:50 +0300

    [tests] dependency tests added

--- tests/test_dependency.py
+++ tests/test_dependency.py
@@ -3,38 +3,110 @@ from pytest import Pytester
 
 status_test_header = """
         import pytest
-
-        from hardpy import get_current_report
-        from hardpy.pytest_hardpy.utils.const import TestStatus as Status
-        from hardpy.pytest_hardpy.utils import NodeInfo
         """
 
 
-def test_dependency(pytester: Pytester, hardpy_opts):
+def test_case_dependency(pytester: Pytester, hardpy_opts):
+    # test_1.py
     pytester.makepyfile(
-        f"""
-        {status_test_header}
+        test_1="""
+        import pytest
+
         def test_one():
             assert True
 
-        @pytest.dependency("test_one")
-        def test_two(request):
+        @pytest.mark.case_dependency("test_1::test_one")
+        def test_two():
             assert True
 
         def test_three():
-            # Тест 3 - отрицательный
             assert False
 
-        def test_four(request):
-            # Тест 4 зависит от теста 3
-            pytest.dependency("test_three")
+        @pytest.mark.case_dependency("test_1::test_three")
+        def test_four():
+            assert False
+
+        @pytest.mark.case_dependency("test_1::test_four")
+        def test_five():
+            assert True 
+    """
+    )
+    result = pytester.runpytest(*hardpy_opts)
+    result.assert_outcomes(passed=2, failed=1, skipped=2)
+
+
+def test_module_dependency(pytester: Pytester, hardpy_opts):
+    # test_1.py
+    pytester.makepyfile(
+        test_1="""
+        import pytest
+
+        def test_one():
             assert True
 
-        def test_five(request):
-            # Тест 5 зависит от теста 4
-            pytest.dependency("test_four")
+        @pytest.mark.case_dependency("test_1::test_one")
+        def test_two():
+            assert True
+
+        def test_three():
+            assert False
+
+        @pytest.mark.case_dependency("test_1::test_three")
+        def test_four():
+            assert False
+
+        @pytest.mark.case_dependency("test_1::test_four")
+        def test_five():
+            assert True 
+    """
+    )
+    pytester.makepyfile(
+        test_2="""
+        import pytest
+
+        pytestmark = pytest.mark.module_dependency("test_1::test_one")
+
+        def test_one():
+            assert True
+
+        def test_two():
+            assert True
+
+        def test_three():
+            assert True
+    """
+    )
+    pytester.makepyfile(
+        test_3="""
+        import pytest
+
+        pytestmark = pytest.mark.module_dependency("test_1::test_three")
+
+        def test_one():
+            assert True
+
+        def test_two():
+            assert True
+
+        def test_three():
+            assert True
+    """
+    )
+    pytester.makepyfile(
+        test_4="""
+        import pytest
+
+        pytestmark = pytest.mark.module_dependency("test_1::test_five")
+
+        def test_one():
+            assert True
+
+        def test_two():
+            assert True
+
+        def test_three():
             assert True
     """
     )
     result = pytester.runpytest(*hardpy_opts)
-    result.assert_outcomes(passed=3)
+    result.assert_outcomes(passed=5, failed=1, skipped=8)
