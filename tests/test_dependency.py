ad2f194ad422346ef0147c8c58e04e185aa3229e
m.antonovich <m.antonovich@thirdpin.io>
2024-05-29 10:53:22 +0300

    [tests] depend dependent from passed test

--- tests/test_dependency.py
+++ tests/test_dependency.py
@@ -39,7 +39,7 @@ def test_case_incorrect_dependency(pytester: Pytester, hardpy_opts):
     """
     )
     result = pytester.runpytest(*hardpy_opts)
-    result.assert_outcomes(passed=1)
+    result.assert_outcomes(passed=2)
 
 
 def test_case_dependency_from_failed(pytester: Pytester, hardpy_opts):
@@ -80,6 +80,28 @@ def test_case_dependency_from_skipped(pytester: Pytester, hardpy_opts):
     )
     result = pytester.runpytest(*hardpy_opts)
     result.assert_outcomes(failed=1, skipped=2)
+
+
+def test_case_dependency_from_not_skipped(pytester: Pytester, hardpy_opts):
+    # test_1.py
+    pytester.makepyfile(
+        test_1="""
+        import pytest
+
+        def test_one():
+            assert True
+
+        @pytest.mark.case_dependency("test_1::test_one")
+        def test_two():
+            assert True
+
+        @pytest.mark.case_dependency("test_1::test_two")
+        def test_five():
+            assert True 
+    """
+    )
+    result = pytester.runpytest(*hardpy_opts)
+    result.assert_outcomes(passed=3)
 
 
 def test_case_dependency(pytester: Pytester, hardpy_opts):
