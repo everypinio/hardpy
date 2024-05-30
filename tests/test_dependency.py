d9848a6490c28d021cfe3b5293ea2dc1dfe1bf96
m.antonovich <m.antonovich@thirdpin.io>
2024-05-30 13:21:56 +0300

    [tests] add tests

--- tests/test_dependency.py
+++ tests/test_dependency.py
@@ -51,6 +51,20 @@ def test_case_dependency_from_incorrect_2(pytester: Pytester, hardpy_opts):
     result.assert_outcomes(errors=1)
 
 
+def test_case_dependency_from_incorrect_3(pytester: Pytester, hardpy_opts):
+    pytester.makepyfile(
+        test_1="""
+        import pytest
+
+        @pytest.mark.case_dependency(":::")
+        def test_one():
+            assert True
+    """
+    )
+    result = pytester.runpytest(*hardpy_opts)
+    result.assert_outcomes(errors=1)
+
+
 def test_case_dependency_from_failed(pytester: Pytester, hardpy_opts):
     pytester.makepyfile(
         test_1="""
@@ -166,7 +180,7 @@ def test_module_dependency_from_incorrect_1(pytester: Pytester, hardpy_opts):
         test_2="""
         import pytest
 
-        pytestmark = pytest.mark.module_dependency("kuhfkhgf")
+        pytestmark = pytest.mark.module_dependency("kuhfkhgf::fghdjshfj")
 
         def test_one():
             assert True
@@ -181,8 +195,22 @@ def test_module_dependency_from_incorrect_2(pytester: Pytester, hardpy_opts):
         test_2="""
         import pytest
 
-        pytestmark = pytest.mark.module_dependency("kuhfkhgf::fghdjshfj")
+        pytestmark = pytest.mark.module_dependency("kuhfkhgf")
 
+        def test_one():
+            assert True
+    """
+    )
+    result = pytester.runpytest(*hardpy_opts)
+    result.assert_outcomes(errors=1)
+
+
+def test_case_dependency_from_module(pytester: Pytester, hardpy_opts):
+    pytester.makepyfile(
+        test_1="""
+        import pytest
+
+        @pytest.mark.case_dependency("test_1")
         def test_one():
             assert True
     """
