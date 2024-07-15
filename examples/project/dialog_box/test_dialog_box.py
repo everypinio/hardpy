8616a4e1b6e6154263a99131bee0104f27260ff5
i.alexandrov <i.alexandrov@thirdpin.io>
2024-07-15 14:06:55 +0300

    [dialog_box] Fix dialog box example

--- examples/project/dialog_box/test_dialog_box.py
+++ examples/project/dialog_box/test_dialog_box.py
@@ -1,11 +1,11 @@
 import pytest
+
 import hardpy
-from hardpy import dialog_box
 
 
 @pytest.mark.case_name("Test base dialog box")
 def test_base_dialog_box():
-    info = dialog_box.DialogBoxData(
+    info = hardpy.dialog_box.DialogBoxData(
         title_bar="Operator check",
         dialog_text="If you are not sleeping, press the Confirm button",
         widget_info=None,
@@ -16,12 +16,12 @@ def test_base_dialog_box():
 
 @pytest.mark.case_name("Test dialog box with text input")
 def test_dialog_box_with_text_input():
-    info = dialog_box.DialogBoxData(
-        title_bar="Example of text input",
+    info = hardpy.dialog_box.DialogBoxData(
         dialog_text="Type some text and press the Confirm button",
-        widget_info=dialog_box.DialogBoxWidget(
-            widget_type=dialog_box.DialogBoxWidgetType.TEXT_INPUT,
-            widget_info={"text": "some text"},
+        title_bar="Example of text input",
+        widget_info=hardpy.dialog_box.DialogBoxWidget(
+            widget_type=hardpy.dialog_box.DialogBoxWidgetType.TEXT_INPUT,
+            widget_info={"text": "Some text"},
         ),
     )
     response = hardpy.run_dialog_box(info)
@@ -30,12 +30,12 @@ def test_dialog_box_with_text_input():
 
 @pytest.mark.case_name("Test dialog box with num input")
 def test_dialog_box_with_num_input():
-    info = dialog_box.DialogBoxData(
-        title_bar="Example of entering a number",
+    info = hardpy.dialog_box.DialogBoxData(
         dialog_text="Enter the number 5 and press the Confirm button",
-        widget_info=dialog_box.DialogBoxWidget(
-            widget_type=dialog_box.DialogBoxWidgetType.NUMERIC_INPUT,
-            widget_info={"text": "enter the number"},
+        title_bar="Example of entering a number",
+        widget_info=hardpy.dialog_box.DialogBoxWidget(
+            widget_type=hardpy.dialog_box.DialogBoxWidgetType.NUMERIC_INPUT,
+            widget_info={"text": "Enter the number"},
         ),
     )
     response = hardpy.run_dialog_box(info)
