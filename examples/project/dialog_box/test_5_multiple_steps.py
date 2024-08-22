806b131028d9420957df7cc1b26b0133ced33754
m.antonovich <m.antonovich@thirdpin.io>
2024-08-22 15:54:27 +0300

    [img] cut images

--- examples/project/dialog_box/test_5_multiple_steps.py
+++ examples/project/dialog_box/test_5_multiple_steps.py
@@ -9,11 +9,16 @@ from hardpy.pytest_hardpy.utils.dialog_box import (
 
 pytestmark = pytest.mark.module_name("Multiple steps dialog box")
 
+
 @pytest.mark.case_name("Multistep")
 def test_multiple_steps():
     steps = [
         StepWidget("Step 1", text="Content for step", widget=None),
-        StepWidget("Step 2", text="Content for step 2", widget=ImageWidget(address="assets/test.png")),
+        StepWidget(
+            "Step 2",
+            text="Content for step 2",
+            widget=ImageWidget(address="assets/test.png"),
+        ),
         StepWidget("Step 3", text=None, widget=ImageWidget(address="assets/test.png")),
     ]
     dbx = DialogBox(
