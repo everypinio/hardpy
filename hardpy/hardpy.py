8546891c5a4761864b4bc6287b721ef6b3803a80
i.alexandrov <i.alexandrov@thirdpin.io>
2024-08-30 17:01:03 +0300

    [hardpy] Add hardpy optional args

--- hardpy/hardpy.py
+++ hardpy/hardpy.py
@@ -1,16 +1,17 @@
 # Copyright (c) 2024 Everypin
 # GNU General Public License v3.0 (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-
 import os
 import sys
 from pathlib import Path
+from typing_extensions import Annotated
+from typing import Optional
 
 import typer
 from uvicorn import run as uvicorn_run
 
 from hardpy.config import ConfigManager
-from hardpy.file_generator import FileGenerator
+from hardpy.template import TemplateGenerator
 
 cli = typer.Typer(add_completion=False)
 default_config = ConfigManager().get_config()
@@ -18,10 +19,10 @@ default_config = ConfigManager().get_config()
 
 @cli.command()
 def init(
-    tests_dir: str = typer.Option(
-        default_config.tests_dir,
-        "-t",
-        help="Specify a tests directory.",
+    tests_dir: Annotated[Optional[str], typer.Argument()] = None,
+    create_database: bool = typer.Option(
+        True,
+        help="Create CouchDB database.",
     ),
     database_user: str = typer.Option(
         default_config.database.user,
@@ -56,9 +57,10 @@ def init(
         help="Specify a socket port.",
     ),
 ):
-    """Initialize HardPy."""
+    """Initialize HardPy tests directory."""
+    _tests_dir = tests_dir if tests_dir else default_config.tests_dir
     ConfigManager().init_config(
-        tests_dir=tests_dir,
+        tests_dir=_tests_dir,
         database_user=database_user,
         database_password=database_password,
         database_host=database_host,
@@ -69,11 +71,12 @@ def init(
         socket_port=socket_port,
     )
     # create tests directory
-    dir_path = Path(Path.cwd() / tests_dir)
+    dir_path = Path(Path.cwd() / _tests_dir)
     os.makedirs(dir_path, exist_ok=True)
 
-    # create database directory
-    os.makedirs(dir_path / "database", exist_ok=True)
+    if create_database:
+        # create database directory
+        os.makedirs(dir_path / "database", exist_ok=True)
 
     # create hardpy.toml
     ConfigManager().create_config(dir_path)
@@ -81,45 +84,49 @@ def init(
     if not config:
         sys.exit()
 
-    fg = FileGenerator(config)
+    template = TemplateGenerator(config)
 
-    # create docker-compose.yaml
-    file_path = Path(dir_path / "docker-compose.yaml")
-    with open(file_path, "w") as docker_compose_yaml:
-        docker_compose_yaml.write(fg.docker_compose_yaml)
+    if create_database:
+        # create docker-compose.yaml
+        file_path = Path(dir_path / "docker-compose.yaml")
+        with open(file_path, "w") as docker_compose_yaml:
+            docker_compose_yaml.write(template.docker_compose_yaml)
 
     # create couchdb.ini
     file_path = Path(dir_path / "database" / "couchdb.ini")
     with open(file_path, "w") as couchdb_ini:
-        couchdb_ini.write(fg.couchdb_ini)
+        couchdb_ini.write(template.couchdb_ini)
 
     # create pytest.ini
     file_path = Path(dir_path / "pytest.ini")
     with open(file_path, "w") as pytest_ini:
-        pytest_ini.write(fg.pytest_ini)
+        pytest_ini.write(template.pytest_ini)
 
     # create test_1.py
     file_path = Path(dir_path / "test_1.py")
     with open(file_path, "w") as test_1_py:  # noqa: WPS114
-        test_1_py.write(fg.test_1_py)
+        test_1_py.write(template.test_1_py)
 
     # create conftest.py
     file_path = Path(dir_path / "conftest.py")
     with open(file_path, "w") as conftest_py:
-        conftest_py.write(fg.conftest_py)
+        conftest_py.write(template.conftest_py)
+
+    print(f"HardPy project {dir_path.name} initialized successfully.")
 
 
 @cli.command()
-def run(
-    tests_dir: str = typer.Option(None, "-t", help="Specify a tests directory."),
-):
+def run(tests_dir: Annotated[Optional[str], typer.Argument()] = None):
     """Run HardPy server."""
     dir_path = Path.cwd() / tests_dir if tests_dir else Path.cwd()
-    print(dir_path)
     config = ConfigManager().read_config(dir_path)
 
     if not config:
+        print(f"Config at path {dir_path} not found.")
         sys.exit()
+
+    print("\nLaunch the HardPy operator panel...")
+    print(f"http://{config.frontend.host}:{config.frontend.port}\n")
 
     uvicorn_run(
         "hardpy.hardpy_panel.api:app",
