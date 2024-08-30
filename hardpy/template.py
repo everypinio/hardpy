3f9a7da894ce72dfc3db4df1801e9f23d32a0bdb
i.alexandrov <i.alexandrov@thirdpin.io>
2024-08-30 16:58:41 +0300

    [hardpy] Change FileGenerator to TemplateGenerator

--- hardpy/template.py
+++ hardpy/template.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024 Everypin
 # GNU General Public License v3.0 (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from hardpy.config import ConfigManager, HardpyConfig
+from hardpy.config import HardpyConfig
 
 docker_compose_yaml = """version: "3.8"
 
@@ -164,7 +164,7 @@ def fill_actions_after_test(post_run_functions: list):
 """
 
 
-class FileGenerator:
+class TemplateGenerator:
 
     def __init__(self, config: HardpyConfig):
         self._config = config
