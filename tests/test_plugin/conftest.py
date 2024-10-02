632ebf0e7e5583cf7f3d532d053b826a5289d225
m.antonovich <m.antonovich@thirdpin.io>
2024-10-02 17:39:13 +0300

    [tests] solve problem with plugin tests path

--- tests/test_plugin/conftest.py
+++ tests/test_plugin/conftest.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 
 from hardpy.common.config import ConfigManager
@@ -7,5 +9,9 @@ pytest_plugins = "pytester"
 
 @pytest.fixture
 def hardpy_opts():
-    config_data = ConfigManager().get_config()
+    config_data = ConfigManager().read_config(
+        Path(__file__).parent.resolve(),
+    )
+    if not config_data:
+        raise Exception("Config not found")  # noqa: EM101, TRY002, TRY003
     return ["--hardpy-db-url", config_data.database.connection_url(), "--hardpy-pt"]
