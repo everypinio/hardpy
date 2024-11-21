a68d9616e882d7e8e78c548390e4fb10ac8699dc
m.antonovich <m.antonovich@thirdpin.io>
2024-11-21 16:35:55 +0300

    [tests] add operator msg

--- examples/dialog_box_sizes/test_1_base_box.py
+++ examples/dialog_box_sizes/test_1_base_box.py
@@ -11,27 +11,27 @@ def test_before():
     assert True
 
 
-@pytest.mark.case_name("Base dialog box with image")
-def test_base_dialog_box_with_image():
-    dbx = DialogBox(
-        title_bar="Operator check",
-        dialog_text="Press the Confirm button",
-        image=ImageWidget(address="assets/test.png", width=50, border=1),
-    )
-    response = run_dialog_box(dbx)
-    assert response
+# @pytest.mark.case_name("Base dialog box with image")
+# def test_base_dialog_box_with_image():
+#     dbx = DialogBox(
+#         title_bar="Operator check",
+#         dialog_text="Press the Confirm button",
+#         image=ImageWidget(address="assets/test.png", width=50, border=1),
+#     )
+#     response = run_dialog_box(dbx)
+#     assert response
 
 
-@pytest.mark.case_name("Base dialog box")
-def test_base_dialog_box():
-    dbx = DialogBox(
-        title_bar="Operator check",
-        dialog_text="Press the Confirm button",
-    )
-    response = run_dialog_box(dbx)
-    assert response
+# @pytest.mark.case_name("Base dialog box")
+# def test_base_dialog_box():
+#     dbx = DialogBox(
+#         title_bar="Operator check",
+#         dialog_text="Press the Confirm button",
+#     )
+#     response = run_dialog_box(dbx)
+#     assert response
 
 
-@pytest.mark.case_name("Empty test after")
-def test_after():
-    assert True
+# @pytest.mark.case_name("Empty test after")
+# def test_after():
+#     assert True
