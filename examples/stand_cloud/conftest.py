7bd533a4be00539dd80f73354512760e067ac189
i.alexandrov <i.alexandrov@thirdpin.io>
2024-11-29 16:20:26 +0300

    [examples] Add StandCloud example

---
+++ examples/stand_cloud/conftest.py
@@ -0,0 +1,42 @@
+import logging
+
+import pytest
+from driver_example import DriverExample  # type: ignore
+
+from hardpy import (
+    StandCloudError,
+    StandCloudLoader,
+    get_current_report,
+    set_operator_message,
+)
+
+
+@pytest.fixture(scope="module")
+def module_log(request: pytest.FixtureRequest):
+    log_name = request.module.__name__
+    yield logging.getLogger(log_name)
+
+
+@pytest.fixture(scope="session")
+def driver_example():
+    example = DriverExample()
+    yield example
+    example.random_method()
+
+
+def finish_executing():
+    report = get_current_report()
+    if report:
+        loader = StandCloudLoader(verify_ssl=False)
+        try:
+            loader.healthcheck()
+            loader.load(report)
+        except StandCloudError as exc:
+            set_operator_message(f"{exc}")
+            return
+
+
+@pytest.fixture(scope="session", autouse=True)
+def fill_actions_after_test(post_run_functions: list):
+    post_run_functions.append(finish_executing)
+    yield
