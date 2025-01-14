e4db091a91a0f2111ddd46f92066627788941325
m.antonovich <m.antonovich@thirdpin.io>
2025-01-14 09:21:17 +0300

    [frontend] add images to operator msg

--- hardpy/hardpy_panel/frontend/src/hardpy_test_view/OperatorMsg.tsx
+++ hardpy/hardpy_panel/frontend/src/hardpy_test_view/OperatorMsg.tsx
@@ -1,17 +1,30 @@
 // Copyright (c) 2024 Everypin
 // GNU General Public License v3.0 (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Classes, Dialog } from "@blueprintjs/core";
 import axios from "axios";
 
 interface StartOperatorMsgDialogProps {
   title: string;
   msg: string;
+  image_base64?: string;
+  image_width?: number;
+  image_border?: number;
 }
 
 export function StartOperatorMsgDialog(props: StartOperatorMsgDialogProps) {
   const [dialogOpen, setDialogOpen] = useState(true);
+  const [imageDimensions, setImageDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
+  const screenWidth = window.screen.width;
+  const screenHeight = window.screen.height;
+  const baseDialogDimensions = { width: 100, height: 100 };
+  const maxSize = 0.6;
+  const minSize = 0.25;
+  const lineHeight = 10;
 
   const handleClose = async () => {
     setDialogOpen(false);
@@ -26,18 +39,68 @@ export function StartOperatorMsgDialog(props: StartOperatorMsgDialogProps) {
     }
   };
 
+  const calculateDimensions = (
+    naturalWidth: number,
+    naturalHeight: number,
+    widthFactor: number
+  ) => ({
+    width: (naturalWidth * (widthFactor || 100)) / 100,
+    height: (naturalHeight * (widthFactor || 100)) / 100,
+  });
+
+  const handleImageLoad = (event: React.SyntheticEvent<HTMLImageElement>) => {
+    const { naturalWidth, naturalHeight } = event.target as HTMLImageElement;
+    setImageDimensions(
+      calculateDimensions(naturalWidth, naturalHeight, props.image_width ?? 100)
+    );
+  };
+
+  const calculateTextLines = (text: string, width: number) => {
+    const context = document.createElement("canvas").getContext("2d");
+    if (context) {
+      context.font = "10px sans-serif";
+      const linesCount = Math.ceil(
+        (text.length * context.measureText("M").width) / width
+      );
+      return linesCount;
+    }
+  };
+
+  const dialogWidth = Math.min(
+    imageDimensions.width + baseDialogDimensions.width,
+    screenWidth * maxSize
+  );
+
+  const textHeight =
+    (calculateTextLines(props.msg, dialogWidth) || 1) * lineHeight;
+
+  const dialogHeight = Math.max(
+    Math.min(
+      imageDimensions.height + baseDialogDimensions.height + textHeight,
+      screenHeight * maxSize
+    ),
+    screenHeight * minSize
+  );
+
+  const imageStyle = {
+    border: `${props.image_border || 0}px solid black`,
+    display: "block",
+    margin: "0 auto",
+  };
+
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
-      const keyboardEvent = event as unknown as React.KeyboardEvent<HTMLInputElement>;
+      const keyboardEvent =
+        event as unknown as React.KeyboardEvent<HTMLInputElement>;
       if (keyboardEvent.key === "Escape") {
         handleClose();
       }
     };
-  
-    window.addEventListener('keydown', handleKeyDown);
-  
+
+    window.addEventListener("keydown", handleKeyDown);
+
     return () => {
-      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener("keydown", handleKeyDown);
     };
   }, []);
 
@@ -51,20 +114,48 @@ export function StartOperatorMsgDialog(props: StartOperatorMsgDialogProps) {
       style={{
         width: "auto",
         height: "auto",
-        minWidth: "300px",
-        minHeight: "200px",
-        maxWidth: "800px",
+        minWidth: screenWidth * minSize,
+        minHeight: screenHeight * minSize,
+        maxWidth: screenWidth * maxSize,
+        maxHeight: screenHeight * maxSize,
+        fontSize: "1rem",
       }}
     >
       <div
         className={Classes.DIALOG_BODY}
-        style={{ wordWrap: "break-word", wordBreak: "break-word" }}
+        style={{
+          wordWrap: "break-word",
+          wordBreak: "break-word",
+          maxHeight: screenHeight * maxSize,
+          overflowY: "auto",
+          maxWidth: screenWidth * maxSize,
+          padding: "10px",
+        }}
       >
         {props.msg.split("\n").map((line, index) => (
           <p key={index} style={{ textAlign: "left" }}>
             {line}
           </p>
         ))}
+        {props.image_base64 && (
+          <div className="image-container">
+            <img
+              src={`data:image/image;base64,${props.image_base64}`}
+              alt="Image"
+              onLoad={handleImageLoad}
+              style={{
+                width: `${props.image_width}%`,
+                height: `${props.image_width}%`,
+                maxWidth: `${dialogWidth - baseDialogDimensions.width / 2}px`,
+                maxHeight: `${dialogHeight - baseDialogDimensions.height / 2}px`,
+                objectFit: "scale-down",
+                transform: `scale(${(props.image_width || 100) / 100})`,
+                transformOrigin: `top center`,
+                ...imageStyle,
+              }}
+            />
+          </div>
+        )}
       </div>
     </Dialog>
   );
--- hardpy/hardpy_panel/frontend/src/hardpy_test_view/SuiteList_v3.tsx
+++ hardpy/hardpy_panel/frontend/src/hardpy_test_view/SuiteList_v3.tsx
@@ -1,149 +1,175 @@
 // Copyright (c) 2024 Everypin
 // GNU General Public License v3.0 (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-import * as React from 'react';
-import _, { Dictionary, } from "lodash"
+import * as React from "react";
+import _, { Dictionary } from "lodash";
 import { H1, H2, H4, Tag, Divider } from "@blueprintjs/core";
 
-import { TestItem, TestSuite as TestSuiteComponent } from "./TestSuite_v3"
-import StartOperatorMsgDialog from 'hardpy_test_view/OperatorMsg';
+import { TestItem, TestSuite as TestSuiteComponent } from "./TestSuite_v3";
+import StartOperatorMsgDialog from "hardpy_test_view/OperatorMsg";
 
 /**
  * Set of suites
  */
 
 /** Name of a suite and an its object */
-interface Suite { name: string; test: TestItem };
-
+interface Suite {
+  name: string;
+  test: TestItem;
+}
 
 interface TestStand {
-    name: string
+  name: string;
 }
 
 interface DriversInfo {
-    driver: Record<string, unknown>
+  driver: Record<string, unknown>;
 }
 
 interface DutInfo {
-    batch: string
-    board_rev: string
+  batch: string;
+  board_rev: string;
 }
 
 interface Dut {
-    serial_number: number
-    info: DutInfo
+  serial_number: number;
+  info: DutInfo;
 }
 
-type Modules = Dictionary<TestItem>
+type Modules = Dictionary<TestItem>;
+
+interface ImageInfo {
+  base64?: string;
+  format?: string;
+  width?: number;
+  border?: number;
+}
 
 interface OperatorMsgProps {
-    msg: string
-    title?: string
-    visible: boolean
+  msg: string;
+  title?: string;
+  visible: boolean;
+  image?: ImageInfo;
 }
 
 export interface TestRunI {
-    modules?: Modules
-    test_stand?: TestStand
-    dut?: Dut
-    name?: string
-    status?: string
-    start_time?: number,
-    timezone?: [string, string],
-    stop_time?: number,
-    progress?: number,
-    drivers?: DriversInfo,
-    artifact?: Record<string, unknown>,
-    operator_msg?: OperatorMsgProps,
+  modules?: Modules;
+  test_stand?: TestStand;
+  dut?: Dut;
+  name?: string;
+  status?: string;
+  start_time?: number;
+  timezone?: [string, string];
+  stop_time?: number;
+  progress?: number;
+  drivers?: DriversInfo;
+  artifact?: Record<string, unknown>;
+  operator_msg?: OperatorMsgProps;
 }
 
 /**
  * SuiteList react component props type
-*/
+ */
 interface Props {
-    db_state: TestRunI
-    defaultClose: boolean
+  db_state: TestRunI;
+  defaultClose: boolean;
 }
 
 /**
  * Render a list of suites with tests inside
  */
 export class SuiteList extends React.Component<Props> {
-    elements_count: number = 0;
+  elements_count: number = 0;
 
-    render(): React.ReactElement {
-
-        if (this.props.db_state.name == undefined) {
-            return <div>
-                <H2>Loading tests... 🤔</H2>
-                {
-                    <H4>Try refreshing the page.</H4>
-                }
-            </div>;
-        }
-
-        const db_state = this.props.db_state;
-        const start = db_state.start_time ? new Date(db_state.start_time * 1000).toLocaleString() : ""
-        const stop = db_state.stop_time ? new Date(db_state.stop_time * 1000).toLocaleString() : ""
-        const start_tz = db_state.timezone ? db_state.timezone : ""
-
-
-        let module_names: string[] = []
-        let modules: Modules = {}
-        if (db_state.modules) {
-            module_names = Object.keys(db_state.modules)
-            modules = db_state.modules
-            this.elements_count = module_names.length
-        }
-
-        const TAG_ELEMENT_STYLE = { margin: 1 };
-
-        return (
-            <>
-                <div>
-                    <H1>
-                        {db_state.name}
-                    </H1>
-                    {db_state.test_stand &&
-                        <Tag minimal style={TAG_ELEMENT_STYLE}>Stand name: {db_state.test_stand?.name}</Tag>
-                    }
-                    {db_state.status &&
-                        <Tag minimal style={TAG_ELEMENT_STYLE}>Status: {db_state.status}</Tag>
-                    }
-                    {start &&
-                        <Tag minimal style={TAG_ELEMENT_STYLE}>Start time: {start + start_tz}</Tag>
-                    }
-                    {stop &&
-                        <Tag minimal style={TAG_ELEMENT_STYLE}>Finish time: {stop + start_tz}</Tag>
-                    }
-                    <Divider />
-                    {_.map(
-                        [...module_names],
-                        (name: string, index: number) => this.suiteRender(index, { name: name, test: modules[name] })
-                    )}
-                </div>
-                <div>
-                    {this.props.db_state.operator_msg && this.props.db_state.operator_msg.msg && this.props.db_state.operator_msg.msg.length > 0 && this.props.db_state.operator_msg.visible == true && (
-                        <StartOperatorMsgDialog
-                            msg={this.props.db_state.operator_msg?.msg}
-                            title={this.props.db_state.operator_msg?.title || "Message"}
-                        />
-                    )}
-                </div>
-            </>
-        );
+  render(): React.ReactElement {
+    if (this.props.db_state.name == undefined) {
+      return (
+        <div>
+          <H2>Loading tests... 🤔</H2>
+          {<H4>Try refreshing the page.</H4>}
+        </div>
+      );
     }
 
-    private suiteRender(index: number, suite: Suite) {
-        return <TestSuiteComponent
-            key={`${suite.name}_${index}`}
-            index={index}
-            test={suite.test}
-            defaultOpen={(this.elements_count < 5) && (this.props.defaultClose == false)}
-            commonTestRunStatus={(this.props.db_state.status)}
-        />
+    const db_state = this.props.db_state;
+    const start = db_state.start_time
+      ? new Date(db_state.start_time * 1000).toLocaleString()
+      : "";
+    const stop = db_state.stop_time
+      ? new Date(db_state.stop_time * 1000).toLocaleString()
+      : "";
+    const start_tz = db_state.timezone ? db_state.timezone : "";
+
+    let module_names: string[] = [];
+    let modules: Modules = {};
+    if (db_state.modules) {
+      module_names = Object.keys(db_state.modules);
+      modules = db_state.modules;
+      this.elements_count = module_names.length;
     }
+
+    const TAG_ELEMENT_STYLE = { margin: 1 };
+
+    return (
+      <>
+        <div>
+          <H1>{db_state.name}</H1>
+          {db_state.test_stand && (
+            <Tag minimal style={TAG_ELEMENT_STYLE}>
+              Stand name: {db_state.test_stand?.name}
+            </Tag>
+          )}
+          {db_state.status && (
+            <Tag minimal style={TAG_ELEMENT_STYLE}>
+              Status: {db_state.status}
+            </Tag>
+          )}
+          {start && (
+            <Tag minimal style={TAG_ELEMENT_STYLE}>
+              Start time: {start + start_tz}
+            </Tag>
+          )}
+          {stop && (
+            <Tag minimal style={TAG_ELEMENT_STYLE}>
+              Finish time: {stop + start_tz}
+            </Tag>
+          )}
+          <Divider />
+          {_.map([...module_names], (name: string, index: number) =>
+            this.suiteRender(index, { name: name, test: modules[name] })
+          )}
+        </div>
+        <div>
+          {this.props.db_state.operator_msg &&
+            this.props.db_state.operator_msg.msg &&
+            this.props.db_state.operator_msg.msg.length > 0 &&
+            this.props.db_state.operator_msg.visible == true && (
+              <StartOperatorMsgDialog
+                msg={this.props.db_state.operator_msg?.msg}
+                title={this.props.db_state.operator_msg?.title || "Message"}
+                image_base64={this.props.db_state.operator_msg?.image?.base64}
+                image_width={this.props.db_state.operator_msg?.image?.width}
+                image_border={this.props.db_state.operator_msg?.image?.border}
+              />
+            )}
+        </div>
+      </>
+    );
+  }
+
+  private suiteRender(index: number, suite: Suite) {
+    return (
+      <TestSuiteComponent
+        key={`${suite.name}_${index}`}
+        index={index}
+        test={suite.test}
+        defaultOpen={
+          this.elements_count < 5 && this.props.defaultClose == false
+        }
+        commonTestRunStatus={this.props.db_state.status}
+      />
+    );
+  }
 }
-
 
 export default SuiteList;
