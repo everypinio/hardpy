f759e1c6d2ac0a160b1361d0aa0805545ba9f640
i.alexandrov <i.alexandrov@thirdpin.io>
2025-01-17 09:47:27 +0300

    [schema, documentation] Add visible and id info

--- hardpy/pytest_hardpy/db/schema/v1.py
+++ hardpy/pytest_hardpy/db/schema/v1.py
@@ -42,7 +42,9 @@ class CaseStateStore(IBaseResult):
               "text": "some text"
             },
             "type": "textinput"
-          }
+          },
+          visible: true,
+          id: "af6ac3e7-7ce8-4a6b-bb9d-88c3e10b5c7a"
         }
       }
     }
@@ -241,7 +243,8 @@ class ResultStateStore(IBaseResult):
       "operator_msg": {
         "msg": "Operator message",
         "title": "Message",
-        "visible": "True"
+        "visible": true,
+        "id": "f45ac1e7-2ce8-4a6b-bb9d-8863e30bcc78"
       },
       "modules": {
         "test_1_a": {
@@ -266,7 +269,9 @@ class ResultStateStore(IBaseResult):
                     "text": "some text"
                   },
                   "type": "textinput"
-                }
+                },
+                visible: true,
+                id: "f45bc1e7-2c18-4a4b-2b9d-8863e30bcc78"
               }
             },
             "test_minute_parity": {
