bb59d16544f50cc8835539db2799651e253f1636
m.antonovich <m.antonovich@thirdpin.io>
2025-01-22 16:52:10 +0300

    [frontend] add font size to steps, checkboxes and radio

--- hardpy/hardpy_panel/frontend/src/hardpy_test_view/DialogBox.tsx
+++ hardpy/hardpy_panel/frontend/src/hardpy_test_view/DialogBox.tsx
@@ -391,6 +391,7 @@ export function StartConfirmationDialog(props: Props) {
                   checked={selectedRadioButton === option}
                   onChange={() => setSelectedRadioButton(option)}
                   onKeyDown={handleKeyDown}
+                  fontSize={`${props.font_size}px`}
                   autoFocus={option === (props.widget_info?.fields ?? [])[0]}
                 />
               ))}
@@ -407,6 +408,7 @@ export function StartConfirmationDialog(props: Props) {
                   checked={selectedCheckboxes.includes(option)}
                   autoFocus={option === (props.widget_info?.fields ?? [])[0]}
                   onKeyDown={handleKeyDown}
+                  fontSize={`${props.font_size}px`}
                   onChange={() => {
                     if (selectedCheckboxes.includes(option)) {
                       setSelectedCheckboxes(
@@ -427,6 +429,7 @@ export function StartConfirmationDialog(props: Props) {
                 id={step.info?.title}
                 key={step.info?.title}
                 title={step.info?.title}
+                fontSize={`${props.font_size}px`}
                 panel={
                   <div className="step-container">
                     <div className="step-content">
