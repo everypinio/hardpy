fd343e91998d52f0b560f3b84eaccd1222b7ace2
m.antonovich <m.antonovich@thirdpin.io>
2025-02-10 10:08:48 +0300

    [examples] correct plot example

--- examples/html_plot/conftest.py
+++ examples/html_plot/conftest.py
@@ -1,9 +1,6 @@
-import os
-import tempfile
-
 import matplotlib.pyplot as plt
 import pytest
-from bokeh.io import output_file, save
+from bokeh.io import export
 from bokeh.plotting import figure
 
 
@@ -18,13 +15,4 @@ def generate_bokeh_html():
     p = figure(width=400, height=400)
     p.line([1, 2, 3, 4], [10, 20, 25, 30], line_width=2)
 
-    with tempfile.NamedTemporaryFile(suffix=".html", delete=False) as tmp_file:
-        output_file(tmp_file.name)
-        save(p)
-
-        with open(tmp_file.name, "r") as f:  # noqa: PTH123, UP015
-            html_string = f.read()
-
-    os.unlink(tmp_file.name)  # noqa: PTH108
-
-    return html_string
+    return export.get_layout_html(p)
