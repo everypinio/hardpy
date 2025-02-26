32ee6d4cc138a37548d887b796f61d30c2239a86
i.alexandrov <i.alexandrov@thirdpin.io>
2025-02-26 16:58:39 +0300

    [stand_cloud] Add http status code to loader

--- examples/stand_cloud/conftest.py
+++ examples/stand_cloud/conftest.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 import pytest
 from driver_example import DriverExample  # type: ignore
 
@@ -20,11 +22,15 @@ def finish_executing():
     if report:
         loader = StandCloudLoader()
         try:
-            loader.healthcheck()
-            loader.load(report)
+            response = loader.load(report)
+            if response.status_code != HTTPStatus.CREATED:
+                msg = (
+                    "Report not uploaded to StandCloud, "
+                    f"status code: {response.status_code}, text: {response.text}",
+                )
+                set_operator_message(msg[0])
         except StandCloudError as exc:
             set_operator_message(f"{exc}")
-            return
 
 
 @pytest.fixture(scope="session", autouse=True)
