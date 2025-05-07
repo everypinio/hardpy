69e686081dabd374993af1049dcecdd1bf59af1b
i.alexandrov <i.alexandrov@thirdpin.io>
2025-05-07 17:21:30 +0300

    [test_dependencies] Fix assert result

--- tests/test_dependencies/test_error_handling.py
+++ tests/test_dependencies/test_error_handling.py
@@ -2,16 +2,15 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from pytest import ExitCode
+
 if TYPE_CHECKING:
     from pytest import Pytester
 
-status_test_header = """
-        import pytest
-        """
-
 
 def test_case_dependency_with_incorrect_data(
-    pytester: Pytester, hardpy_opts: list[str],
+    pytester: Pytester,
+    hardpy_opts: list[str],
 ):
     pytester.makepyfile(
         test_1="""
@@ -22,67 +21,7 @@ def test_case_dependency_with_incorrect_data(
     """,
     )
     result = pytester.runpytest(*hardpy_opts)
-    result.assert_outcomes()
-
-
-def test_module_dependency_with_incorrect_case(
-    pytester: Pytester,
-    hardpy_opts: list[str],
-):
-    pytester.makepyfile(
-        test_1="""
-        import pytest
-
-        def test_one():
-            assert True
-    """,
-    )
-    pytester.makepyfile(
-        test_2="""
-        import pytest
-
-        pytestmark = pytest.mark.dependency("test_1::fghdjshfj")
-
-        def test_one():
-            assert True
-    """,
-    )
-    result = pytester.runpytest(*hardpy_opts)
-    result.assert_outcomes()
-
-
-def test_case_dependency_with_incorrect_module_and_case(
-    pytester: Pytester,
-    hardpy_opts: list[str],
-):
-    pytester.makepyfile(
-        test_1="""
-        import pytest
-
-        @pytest.mark.dependency("kuhfkhgf::qwqw")
-        def test_one():
-            assert True
-    """,
-    )
-    result = pytester.runpytest(*hardpy_opts)
-    result.assert_outcomes()
-
-
-def test_case_dependency_with_incorrect_module(
-    pytester: Pytester,
-    hardpy_opts: list[str],
-):
-    pytester.makepyfile(
-        test_1="""
-        import pytest
-
-        @pytest.mark.dependency("kuhfkhgf")
-        def test_one():
-            assert True
-    """,
-    )
-    result = pytester.runpytest(*hardpy_opts)
-    result.assert_outcomes()
+    assert result.ret == ExitCode.NO_TESTS_COLLECTED
 
 
 def test_module_dependency_with_incorrect_data(
@@ -108,7 +47,75 @@ def test_module_dependency_with_incorrect_data(
     """,
     )
     result = pytester.runpytest(*hardpy_opts)
-    result.assert_outcomes()
+    assert result.ret == ExitCode.NO_TESTS_COLLECTED
+
+
+def test_module_dependency_with_incorrect_case(
+    pytester: Pytester,
+    hardpy_opts: list[str],
+):
+    pytester.makepyfile(
+        test_1="""
+        import pytest
+
+        def test_one():
+            assert True
+    """,
+    )
+    pytester.makepyfile(
+        test_2="""
+        import pytest
+
+        pytestmark = pytest.mark.dependency("test_1::fghdjshfj")
+
+        def test_one():
+            assert True
+    """,
+    )
+    result = pytester.runpytest(*hardpy_opts)
+    result.assert_outcomes(passed=2)
+
+
+def test_case_dependency_with_incorrect_module_and_case(
+    pytester: Pytester,
+    hardpy_opts: list[str],
+):
+    pytester.makepyfile(
+        test_1="""
+        import pytest
+
+        @pytest.mark.dependency("kuhfkhgf::qwqw")
+        def test_one():
+            assert True
+
+        @pytest.mark.dependency("kuhfkhgf::qwqw")
+        def test_two():
+            assert False
+    """,
+    )
+    result = pytester.runpytest(*hardpy_opts)
+    result.assert_outcomes(passed=1, failed=1)
+
+
+def test_case_dependency_with_incorrect_module(
+    pytester: Pytester,
+    hardpy_opts: list[str],
+):
+    pytester.makepyfile(
+        test_1="""
+        import pytest
+
+        @pytest.mark.dependency("kuhfkhgf")
+        def test_one():
+            assert True
+
+        @pytest.mark.dependency("kuhfkhgf")
+        def test_two():
+            assert False
+    """,
+    )
+    result = pytester.runpytest(*hardpy_opts)
+    result.assert_outcomes(passed=1, failed=1)
 
 
 def test_module_dependency_with_incorrect_module_and_correct_case(
@@ -134,4 +141,4 @@ def test_module_dependency_with_incorrect_module_and_correct_case(
     """,
     )
     result = pytester.runpytest(*hardpy_opts)
-    result.assert_outcomes()
+    result.assert_outcomes(passed=2)
