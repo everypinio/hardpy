d0ea44618aefa132403a4e80f8399adf4b25f2fe
i.alexandrov <i.alexandrov@thirdpin.io>
2025-05-16 10:58:32 +0300

    [cli] Rm socket SO_REUSEADDR flag

--- hardpy/cli/cli.py
+++ hardpy/cli/cli.py
@@ -153,7 +153,6 @@ def run(tests_dir: Annotated[Optional[str], typer.Argument()] = None) -> None:
     print("\nLaunch the HardPy operator panel...")
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         try:
             s.bind(("127.0.0.1", int(config.frontend.port)))
         except ValueError:
