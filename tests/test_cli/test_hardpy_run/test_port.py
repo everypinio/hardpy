06b13a229968255d0d626c06349f90ab74a406c0
i.alexandrov <i.alexandrov@thirdpin.io>
2025-05-16 10:59:17 +0300

    [test_hardpy_run] Return process in single folder

--- tests/test_cli/test_hardpy_run/test_port.py
+++ tests/test_cli/test_hardpy_run/test_port.py
@@ -33,24 +33,19 @@ def test_single_start(process_killer: NoneType, tmp_path: Path):
     assert process.wait(3) is not None
 
 def test_repeated_start(process_killer: NoneType, tmp_path: Path):
-    test_dir_1 = _init_test_project(tmp_path, "test_1")
-    RUN_CMD[2] = test_dir_1
+    test_dir = _init_test_project(tmp_path, "test_1")
+    RUN_CMD[2] = test_dir
 
-    process = Popen(RUN_CMD, stdout=PIPE, stderr=PIPE)
+    process_1 = Popen(RUN_CMD, stdout=PIPE, stderr=PIPE)
 
-    if ERROR_STR in _collect_n_string(process=process, n=5):
+    if ERROR_STR in _collect_n_string(process=process_1, n=5):
         pytest.fail(ERROR_STR)
 
-    test_dir_2 = _init_test_project(tmp_path, "test_2")
-    RUN_CMD[2] = test_dir_2
+    process_2 = Popen(RUN_CMD, stdout=PIPE, stderr=PIPE)
+    assert ERROR_STR in _collect_n_string(process=process_2, n=5)
 
-    process = Popen(RUN_CMD, stdout=PIPE, stderr=PIPE)
-
-    assert ERROR_STR in _collect_n_string(process=process, n=5)
-
-    psutil.Process(process.pid).kill()
-    assert process.wait(3) is not None
-
+    psutil.Process(process_1.pid).kill()
+    assert process_1.wait(3) is not None
 
 
 def test_start_after_stop_single_port(process_killer: NoneType, tmp_path: Path):
