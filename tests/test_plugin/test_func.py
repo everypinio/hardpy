76ff213d39356ec6dff72813628ca4a5005b8215
i.alexandrov <alexandroviliav@gmail.com>
2025-08-20 15:41:32 +0300

    [test_plugin] Add test_dut_sub_unit

--- tests/test_plugin/test_func.py
+++ tests/test_plugin/test_func.py
@@ -158,6 +158,66 @@ def test_empty_dut_info(pytester: Pytester, hardpy_opts: list[str]):
     result.assert_outcomes(passed=1)
 
 
+def test_dut_sub_unit(pytester: Pytester, hardpy_opts: list[str]):
+    pytester.makepyfile(
+        f"""
+        {func_test_header}
+        from hardpy import set_sub_unit, SubUnit
+
+        def test_sub_unit():
+            report = hardpy.get_current_report()
+            assert report.dut.sub_units == [], "Sub units is not empty before start."
+
+            serial_number = str(uuid4())[:6]
+            part_number = "part_number_1"
+            name = "Test Device"
+            type = "PCBA"
+            revision = "REV1.0"
+            info = {{"sw_version": "1.0"}}
+            sub_unit = SubUnit(
+                serial_number=serial_number,
+                part_number=part_number,
+                name=name,
+                type=type,
+                revision=revision,
+                info=info,
+            )
+            hardpy.set_sub_unit(sub_unit)
+            report = hardpy.get_current_report()
+            assert serial_number == report.dut.sub_units[0].serial_number
+            assert part_number == report.dut.sub_units[0].part_number
+            assert name == report.dut.sub_units[0].name
+            assert type == report.dut.sub_units[0].type
+            assert revision == report.dut.sub_units[0].revision
+            assert info == report.dut.sub_units[0].info
+
+            serial_number_2 = str(uuid4())[:6]
+            part_number_2 = "part_number_2"
+            name_2 = "Test Device 2"
+            type_2 = "PCBA"
+            revision_2 = "REV2.0"
+
+            sub_unit_2 = SubUnit(
+                serial_number=serial_number_2,
+                part_number=part_number_2,
+                name=name_2,
+                type=type_2,
+                revision=revision_2,
+            )
+            hardpy.set_sub_unit(sub_unit_2)
+            report = hardpy.get_current_report()
+            assert serial_number_2 == report.dut.sub_units[1].serial_number
+            assert part_number_2 == report.dut.sub_units[1].part_number
+            assert name_2 == report.dut.sub_units[1].name
+            assert type_2 == report.dut.sub_units[1].type
+            assert revision_2 == report.dut.sub_units[1].revision
+            assert {{}} == report.dut.sub_units[1].info
+    """,
+    )
+
+    result = pytester.runpytest(*hardpy_opts)
+    result.assert_outcomes(passed=1)
+
 def test_stand_name(pytester: Pytester, hardpy_opts: list[str]):
     pytester.makepyfile(
         f"""
