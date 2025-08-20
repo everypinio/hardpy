ccc1e188b20808d377132c251414bb9234aef075
i.alexandrov <alexandroviliav@gmail.com>
2025-08-20 14:05:35 +0300

    [utils] Rename module to stand_type

--- hardpy/pytest_hardpy/utils/stand_type.py
+++ hardpy/pytest_hardpy/utils/stand_type.py
@@ -6,8 +6,6 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Mapping  # noqa: UP035
 
-from hardpy.pytest_hardpy.db import DatabaseField as DF  # noqa: N817
-
 if TYPE_CHECKING:
     from datetime import datetime
 
@@ -24,29 +22,12 @@ class Instrument:
 
 
 @dataclass
-class DUT:
-    """DUT."""
+class SubUnit:
+    """Sub unit of DUT."""
 
     name: str | None = None
     type: str | None = None
     serial_number: str | None = None
     part_number: str | None = None
     revision: str | None = None
-    sub_duts: list[DUT] | None = None
     info: Mapping[str, str | int | float | datetime] | None = None
-
-    def to_dict(self) -> dict:
-        """Convert ImageComponent to dictionary.
-
-        Returns:
-            dict: ImageComponent dictionary
-        """
-        return {
-            DF.NAME: self.name,
-            DF.TYPE: self.type,
-            DF.SERIAL_NUMBER: self.serial_number,
-            DF.PART_NUMBER: self.part_number,
-            DF.REVISION: self.revision,
-            DF.SUB_DUTS: self.sub_duts if self.sub_duts else [],
-            DF.INFO: self.info,
-        }
