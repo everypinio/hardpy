40a28da336259189ac39df36d9c0202e1fac2811
@m.antonovich <m.antonovich@thirdpin.io>
2025-09-11 14:58:02 +0300

    [frontend] remove comment

--- hardpy/hardpy_panel/frontend/src/hardpy_test_view/SuiteList.tsx
+++ hardpy/hardpy_panel/frontend/src/hardpy_test_view/SuiteList.tsx
@@ -118,7 +118,6 @@ export class SuiteList extends React.Component<
   }
 
   componentDidUpdate(prevProps: Props) {
-    // Если имя теста изменилось, очищаем сохраненные закрытые сообщения
     if (prevProps.db_state.name !== this.props.db_state.name) {
       try {
         localStorage.removeItem(CLOSED_MESSAGES_KEY);
