12d85e5e5e3b6fd5264d8ae51d4a95ed97c2f151
i.alexandrov <alexandroviliav@gmail.com>
2025-10-03 17:17:27 +0300

    [db] Rename cloudstore to tempstore

--- hardpy/pytest_hardpy/db/tempstore.py
+++ hardpy/pytest_hardpy/db/tempstore.py
@@ -1,20 +1,25 @@
 # Copyright (c) 2025 Everypin
 # GNU General Public License v3.0 (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import annotations
 
 from logging import getLogger
+from typing import TYPE_CHECKING
 
-from pycouchdb.exceptions import Conflict
+from pycouchdb.exceptions import Conflict, NotFound
 
 from hardpy.common.singleton import SingletonMeta
 from hardpy.pytest_hardpy.db.base_store import BaseStore
 from hardpy.pytest_hardpy.db.schema import ResultRunStore
 
+if TYPE_CHECKING:
+    from collections.abc import Generator
 
-class CloudStore(BaseStore, metaclass=SingletonMeta):
+
+class TempStore(BaseStore, metaclass=SingletonMeta):
     """HardPy temporary storage for data syncronization."""
 
     def __init__(self) -> None:
-        super().__init__("cloudstore")
+        super().__init__("tempstore")
         self._log = getLogger(__name__)
         self._doc: dict = self._init_doc()
         self._schema = ResultRunStore
@@ -23,7 +28,6 @@ class CloudStore(BaseStore, metaclass=SingletonMeta):
         """Push report to the report database."""
         report_dict = report.model_dump()
         report_id = report_dict.pop("id")
-        report_dict["_id"] = report_id
         try:
             self._db.save(report_dict)
         except Conflict as exc:
@@ -31,3 +35,19 @@ class CloudStore(BaseStore, metaclass=SingletonMeta):
             return False
         self._log.debug(f"Report saved with id: {report_id}")
         return True
+
+    def reports(self) -> Generator[ResultRunStore]:
+        """Get all reports from the report database."""
+        yield from self._db.all()
+
+    def delete(self, report_id: str) -> bool:
+        """Delete report from the report database."""
+        try:
+            self._db.delete(report_id)
+        except (NotFound, Conflict):
+            return False
+        return True
+
+    def dict_to_schema(self, report: dict) -> ResultRunStore:
+        """Convert report dict to report schema."""
+        return self._schema(**report)
