f14770c9562669b5d88669a9e18d248839e6ab65
i.alexandrov <alexandroviliav@gmail.com>
2025-12-09 14:59:01 +0300

    [stand_cloud_integration] Rm excess consts

--- examples/stand_cloud_integration/integration_flow.py
+++ examples/stand_cloud_integration/integration_flow.py
@@ -2,16 +2,14 @@ import sys
 
 import requests
 
+SSL_VERIFY = True
+
 ########################################
 # Change API_KEY to your api key
 API_KEY = "your_api_key"
 ########################################
 
-SSL_VERIFY = True
-BASE_URL = "https://standcloud.io"
-API_URL = BASE_URL + "/integration/api/v1"  # API endpoints
-
-HEALTHCHECK_URL = API_URL + "/healthcheck"
+HEALTHCHECK_URL = "https://standcloud.io/integration/api/v1/healthcheck"
 response = requests.get(HEALTHCHECK_URL, verify=SSL_VERIFY)
 
 if response.status_code != 200:
@@ -25,9 +23,8 @@ header = {
     "Content-type": "application/json",
     "Accept": "text/plain",
 }
-print("\nheader: ", header)
 
-USER_INFO_URL = API_URL + "/test_run"
+USER_INFO_URL = "https://standcloud.io/integration/api/v1/test_run"
 response = requests.get(USER_INFO_URL, headers=header, verify=SSL_VERIFY)
 
 if response.status_code != 200:
