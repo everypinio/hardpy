9f6a2a5f0697521d4c48bf0bee628896972c7548
i.alexandrov <alexandroviliav@gmail.com>
2025-12-29 15:59:37 +0300

    [test_plugin] Add JSON tests

--- tests/test_plugin/conftest.py
+++ tests/test_plugin/conftest.py
@@ -7,18 +7,26 @@ from hardpy.common.config import ConfigManager
 pytest_plugins = "pytester"
 
 
-@pytest.fixture
-def hardpy_opts():
+@pytest.fixture(params=["couchdb", "json"], autouse=True)
+def hardpy_opts(request):  # noqa: ANN001
     config_manager = ConfigManager()
-    config_data = config_manager.read_config(
-        Path(__file__).parent.resolve(),
-    )
-    if not config_data:
-        msg = "Config not found"
-        raise RuntimeError(msg)
-    return [
-        "--hardpy-clear-database",
-        "--hardpy-db-url",
-        config_data.database.url,
-        "--hardpy-pt",
-    ]
+    if request.param == "couchdb":
+        config_data = config_manager.read_config(
+            Path(__file__).parent.resolve(),
+        )
+        if not config_data:
+            msg = "Config not found"
+            raise RuntimeError(msg)
+
+        return [
+            "--hardpy-clear-database",
+            "--hardpy-db-url",
+            config_data.database.url,
+            "--hardpy-pt",
+        ]
+    if request.param == "json":
+        config_data = config_manager.read_config(
+            Path(__file__).parent / "json_toml",
+        )
+        return [ "--hardpy-clear-database", "--hardpy-pt"]
+    return None
