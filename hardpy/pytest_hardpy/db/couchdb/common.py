27143e0ac73c188946d17b89887fe95f90a69c8d
i.alexandrov <alexandroviliav@gmail.com>
2026-03-25 10:02:37 +0300

    [couchdb] Add doc updating retries

--- hardpy/pytest_hardpy/db/couchdb/common.py
+++ hardpy/pytest_hardpy/db/couchdb/common.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2026 Everypin
 # GNU General Public License v3.0 (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from time import sleep
+
 from pycouchdb import Server as DbServer
 from pycouchdb.exceptions import Conflict
 
@@ -13,7 +15,18 @@ def update_db(doc: dict, doc_id: str, db: DbServer) -> None:
         doc["_rev"] = db.get(doc_id)["_rev"]
         doc = db.save(doc)
 
+
 def update_doc(doc: dict, doc_id: str, db: DbServer) -> dict:
     """Reload document from storage backend to memory."""
-    doc["_rev"] = db.get(doc_id)["_rev"]
-    return db.get(doc_id)
+    retries = 5
+    latest_doc = db.get(doc_id)
+    for attempt in range(retries):
+        try:
+            doc["_rev"] = latest_doc["_rev"]
+            break
+        except KeyError:
+            sleep(0.1 * (attempt + 1))
+            if attempt == retries - 1:
+                raise
+            latest_doc = db.get(doc_id)
+    return latest_doc
