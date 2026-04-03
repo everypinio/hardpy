dcd16a6b192e1ede3b48362db9bb3b363b7d85ab
i.alexandrov <alexandroviliav@gmail.com>
2026-04-03 14:08:51 +0300

    [common] Add PID check for multiprocessing

--- hardpy/common/singleton.py
+++ hardpy/common/singleton.py
@@ -2,13 +2,14 @@
 # GNU General Public License v3.0 (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import annotations
 
+import os
 from typing import Generic, TypeVar
 
 T = TypeVar("T")
 
 
 class SingletonMeta(type, Generic[T]):  # noqa: D101
-    _instances: dict[SingletonMeta[T], T] = {}  # noqa: RUF012
+    _instances: dict[SingletonMeta[T], tuple[T, int]] = {}  # noqa: RUF012
 
     def __call__(cls, *args, **kwargs) -> T:  # noqa: ANN002, ANN003
         """Magic method to create an instance of the class.
@@ -19,12 +20,13 @@ class SingletonMeta(type, Generic[T]):  # noqa: D101
         Returns:
             object: An instance of the class
         """
-        if cls not in cls._instances:
-            cls._instances[cls] = super().__call__(
-                *args,
-                **kwargs,
-            )
-        return cls._instances[cls]
+        # Check PID to avoid race conditions in multiprocessing
+        current_pid = os.getpid()
+        if cls not in cls._instances or cls._instances[cls][1] != current_pid:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = (instance, current_pid)
+
+        return cls._instances[cls][0]
 
     def __instancecheck__(cls, instance: object) -> bool:
         return cls.__subclasscheck__(type(instance))
